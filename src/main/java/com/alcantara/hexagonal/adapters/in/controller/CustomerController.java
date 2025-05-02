59b72ad05415f56060e750b475050bc81dbfa760
joao.alcantara <joao.alcantara@pdcase.com.br>
2025-05-02 14:19:30 -0300

    Adiciona crud - delete customer

--- src/main/java/com/alcantara/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/alcantara/hexagonal/adapters/in/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.alcantara.hexagonal.adapters.in.controller;
 import com.alcantara.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.alcantara.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.alcantara.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.alcantara.hexagonal.application.ports.in.DeleteCustomerByIdInputPort;
 import com.alcantara.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.alcantara.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.alcantara.hexagonal.application.ports.in.UpdateCustomerInputPort;
@@ -23,6 +24,9 @@ public class CustomerController {
 
     @Autowired
     private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private DeleteCustomerByIdInputPort deleteCustomerByIdInputPort;
 
     @Autowired
     private CustomerMapper customerMapper;
@@ -46,6 +50,12 @@ public class CustomerController {
         var customer = customerMapper.toCustomer(customerRequest);
         customer.setId(id);
         updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable final String id) {
+        deleteCustomerByIdInputPort.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/alcantara/hexagonal/application/ports/out/DeleteCustomerByIdOutputPort.java
@@ -0,0 +1,6 @@
+package com.alcantara.hexagonal.application.ports.out;
+
+public interface DeleteCustomerByIdOutputPort {
+
+    void delete(String id);
+}
